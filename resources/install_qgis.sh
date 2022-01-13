ec60319d216ab8979bc35d801e14b488f3776600
matt <matt@kasmweb.com>
2022-01-13 14:22:23 +0000

    fix qgis for x86

--- resources/install_qgis.sh
+++ resources/install_qgis.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-ARCH=$(uname -p)
-
-if [[ "${ARCH}" =~ ^aarch64$ ]] ; then
-    apt-get update
-    apt-get install -y qgis qgis-plugin-grass
-else
-    apt-get update
-    apt-get install -y gnupg software-properties-common
-    wget -qO - https://qgis.org/downloads/qgis-2020.gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import
-    chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
-    add-apt-repository "deb https://qgis.org/debian `lsb_release -c -s` main"
-    apt-get update
-    apt-get install -y qgis qgis-plugin-grass
-fi
+apt-get update
+apt-get install -y qgis qgis-plugin-grass
